be58dbd322720f124dcf03ebb791c70854bd2e70
James Nguyen <jamesnguyen.tech@gmail.com>
2025-01-21 16:46:34 +0700

    check spl token balance

--- programs/amm-basein/src/lib.rs
+++ programs/amm-basein/src/lib.rs
@@ -1,11 +1,14 @@
 use anchor_lang::prelude::*;
 use instructions::*;
+use anchor_spl::{
+    token::TokenAccount
+};
 
 pub mod errors;
 pub mod instructions;
 pub mod utils;
 
-declare_id!("En4ePE1rLRxZHFe7c49ChZE3wwJAfdELNjWW9yydxfFA");
+declare_id!("Aaz9GePyuVMTozYh2sXuYLaSHhubE8nYuwBqxSbGHo89");
 
 #[program]
 pub mod amm_basein {
@@ -17,7 +20,19 @@ pub mod amm_basein {
         amount_in: u64,
         minimum_amount_out: u64,
     ) -> Result<()> {
-        instructions::swap_base_in(ctx, amount_in, minimum_amount_out)
+        let clone_account = ctx.accounts.user_token_source.clone();
+        let mut account_data = clone_account.try_borrow_mut_data()?;
+        let mut token_account = TokenAccount::try_deserialize(&mut account_data.as_ref()).expect("Error Deserializing Data");
+        msg!("balance before: {}", token_account.amount);
+        // Release the borrow before moving ctx
+        drop(account_data);
+
+        instructions::swap_base_in(ctx, amount_in, minimum_amount_out);
+
+        account_data = clone_account.try_borrow_mut_data()?;
+        token_account = TokenAccount::try_deserialize(&mut account_data.as_ref()).expect("Error Deserializing Data");
+        msg!("balance after: {}", token_account.amount);
+        Ok(())
     }
     /// swap_base_in instruction
     pub fn proxy_swap_dlmm<'info>(
